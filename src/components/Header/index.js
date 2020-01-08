249b865bb7a2c5d3d2302a4904a9b41413d7616c
FlavioBS <bzsflavio@gmail.com>
2020-01-08 01:31:49 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+// import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <header>
+      <div>HELLO</div>
+      <nav>xxxxx</nav>
+    </header>
+  )
+}
